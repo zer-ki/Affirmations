a315414a41dff1f76448e42398d63258902f3816
Filip <filipkarcz01@gmail.com>
2023-04-20 19:58:24 +0200

    Fix typo

--- app/src/main/java/com/example/affirmations/data/Datasource.kt
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -9,7 +9,7 @@ class Datasource {
         return listOf<Affirmation>(
             Affirmation(R.string.affirmation1, R.drawable.image1),
             Affirmation(R.string.affirmation2, R.drawable.image2),
-            Affirmation(R.string.affirmation3, R.drawable.image1),
+            Affirmation(R.string.affirmation3, R.drawable.image3),
             Affirmation(R.string.affirmation4, R.drawable.image4),
             Affirmation(R.string.affirmation5, R.drawable.image5),
             Affirmation(R.string.affirmation6, R.drawable.image6),
