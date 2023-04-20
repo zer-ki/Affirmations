b932f3283b9921cf1930de4f1c3ce2f264f32e58
Filip <filipkarcz01@gmail.com>
2023-04-20 19:35:18 +0200

    Add imageView to layout

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,4 +1,7 @@
 package com.example.affirmations.model
 
-data class Affirmation(val stringResourceId: Int, val imageResourceId: Int) {
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(@StringRes val stringResourceId: Int, @DrawableRes val imageResourceId: Int) {
 }
