22e6fd4c1a311d2454ffe98c3cfdc3ce9acada82
Filip <filipkarcz01@gmail.com>
2023-04-24 18:24:18 +0200

    Add scroll to item test

--- app/src/androidTest/java/com/example/affirmations/AffirmationsListsTest.kt
+++ app/src/androidTest/java/com/example/affirmations/AffirmationsListsTest.kt
@@ -1,5 +1,10 @@
 package com.example.affirmations
 
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import org.junit.Rule
 import org.junit.Test
@@ -7,13 +12,18 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
-@get: Rule
-val activity = ActivityScenarioRule(MainActivty::class.java)
+
 
 class AffirmationsListsTest {
+    @get: Rule
+    val activity = ActivityScenarioRule(MainActivity::class.java)
     @Test
     fun scroll_to_item(){
-
+        onView(withId(R.id.recycler_view)).perform(
+            RecyclerViewActions.scrollTo<RecyclerView.ViewHolder>(withText(R.string.affirmation10))
+        )
+        onView(withText(R.string.affirmation10)).check(matches(isDisplayed()))
     }
+
 
 }
