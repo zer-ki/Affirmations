6987f5f2701b750556cdd42648ec923723eb4c70
Filip <filipkarcz01@gmail.com>
2023-04-24 17:51:20 +0200

    Create list test file

---
+++ app/src/androidTest/java/com/example/affirmations/AffirmationsListsTest.kt
@@ -0,0 +1,19 @@
+package com.example.affirmations
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+@get: Rule
+val activity = ActivityScenarioRule(MainActivty::class.java)
+
+class AffirmationsListsTest {
+    @Test
+    fun scroll_to_item(){
+
+    }
+
+}
